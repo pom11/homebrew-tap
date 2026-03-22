64ff1678a345b44bb9f8b12bc057078ddd41f03d
pom11 <parsecoriginalmastercraft@gmail.com>
2026-03-22 20:14:31 +0200

    Update Radio to v4.4.4

--- Casks/radio.rb
+++ Casks/radio.rb
@@ -1,6 +1,6 @@
 cask "radio" do
-  version "4.4.3"
-  sha256 "b65e6c2e0a01957462aa9ca77c109d1f033afdcf9a9c35f4656a28d37f19bbd4"
+  version "4.4.4"
+  sha256 "4fbfc2e2db45e7bdf74bdcaadeb7d130990cc3083c9b596078013fcdd038fd25"
 
   url "https://github.com/pom11/Radio/releases/download/v#{version}/Radio.dmg"
   name "Radio"
