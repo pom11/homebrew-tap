3d836716f7c1d90b9a3de63f94a710c13150dcde
pom11 <parsecoriginalmastercraft@gmail.com>
2026-03-25 11:48:18 +0200

    Grid 1.1.0

--- Casks/grid.rb
+++ Casks/grid.rb
@@ -1,6 +1,6 @@
 cask "grid" do
-  version "1.0.9"
-  sha256 "f3e91adac0e9e0cb93bb850f85c866bd5f5c7fda707a07a27f872ee8e7ce9ba8"
+  version "1.1.0"
+  sha256 "0098b43221471092b2336b6c7d3738c3be24351f25f581fe397cd6cd0a84b96a"
 
   url "https://github.com/pom11/Grid/releases/download/v#{version}/Grid.dmg"
   name "Grid"
